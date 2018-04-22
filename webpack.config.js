4742bd958b7f661f2bed2dd7a767d373b1becffb
Mohammad Ali Khan <mohammad_ali93@live.com>
2018-04-22 14:06:29 +0100

    Add webpack to the project for compiling and publishing the package (#9)

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'awesome-typescript-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'umd',
+    library: 'randomTest',
+    umdNamedDefine: true,
+  },
+  mode: 'production'
+};
