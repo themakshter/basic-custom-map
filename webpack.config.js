568cc344a5d483367f63376ffcdbf2d978d25c1a
themakshter <mohammad_ali93@live.com>
2018-06-07 00:51:02 +0100

    Include snap-svg in bundle
    To be on the safe side, we are currently going to include the snap-svg
    library in our created bundle. This meant updating webpack.config.js and
    including a few other things

--- webpack.config.js
+++ webpack.config.js
@@ -8,16 +8,20 @@ module.exports = {
         test: /\.tsx?$/,
         use: 'awesome-typescript-loader',
         exclude: /node_modules/
+      },
+      {
+        test: require.resolve('snapsvg/dist/snap.svg.js'),
+        use: 'imports-loader?this=>window,fix=>module.exports=0'
       }
     ]
   },
   resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ]
+    extensions: [ '.tsx', '.ts', '.js' ],
+    alias:{
+      snapsvg: 'snapsvg/dist/snap.svg.js'
+    }
   },
   devtool: 'source-map',
-  externals: {
-    snapsvg: 'Snap'
-  },
   output: {
     filename: 'basic-custom-map.min.js',
     path: path.resolve(__dirname, 'dist'),
