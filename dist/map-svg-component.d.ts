8f62b9e3ca2d9a81d95c3564d8ab4ec315b71947
Mohammad Ali Khan <mohammad_ali93@live.com>
2018-07-26 23:44:48 +0100

    Add coordinate space for map (#41)
    This change allows users of the library to define a coordinate space
    (width and height) on which their pins are based. This means that when
    they can define a dynamic svg component and the map will automatically
    scale the provided components. The demo was also updated to allow playing
    around with the svg component values

--- dist/map-svg-component.d.ts
+++ dist/map-svg-component.d.ts
@@ -3,12 +3,16 @@ import { SvgComponent } from "./svg-component";
 import { Position } from "./position";
 import { RectangleDimensions } from "./rectangle-dimensions";
 import { Map } from "./map";
+import { MapPin } from "./map-pin";
 export declare class MapSvgComponent implements SvgComponent {
     private map;
     snapCtx: Snap.Paper;
     position: Position;
     dimensions: RectangleDimensions;
+    private xScale;
+    private yScale;
     constructor(map: Map, snapCtx: Snap.Paper, position: Position, dimensions: RectangleDimensions);
     draw(): Promise<void>;
-    drawMapPins(mapPins: Array<any>): void;
+    drawMapPins(mapPins: Array<MapPin>): void;
+    getScaledPosition(position: Position): Position;
 }
--- src/map-svg-component.ts
+++ src/map-svg-component.ts
@@ -4,10 +4,15 @@ import { SvgComponent } from "./svg-component";
 import { Position } from "./position";
 import { RectangleDimensions } from "./rectangle-dimensions";
 import { Map } from "./map";
+import { MapPin } from "./map-pin";
 
 export class MapSvgComponent implements SvgComponent {
-
-  constructor(private map: Map, public snapCtx: Snap.Paper, public position: Position, public dimensions: RectangleDimensions){}
+  private xScale: number;
+  private yScale: number;
+  constructor(private map: Map, public snapCtx: Snap.Paper, public position: Position, public dimensions: RectangleDimensions){
+    this.xScale = this.dimensions.width / this.map.coordinateSpace.width;
+    this.yScale = this.dimensions.height / this.map.coordinateSpace.height;
+  }
 
   draw() : Promise<void> {
     return new Promise<void>( (resolve: ()=> any, reject: ()=> any) => {
@@ -19,12 +24,20 @@ export class MapSvgComponent implements SvgComponent {
     });
   }
 
-  drawMapPins(mapPins: Array<any>): void {
+  drawMapPins(mapPins: Array<MapPin>): void {
     for(let pin of mapPins){
       let pinSize: number = this.dimensions.width * 0.075;
       let pinDimensions: RectangleDimensions = {width: pinSize, height: pinSize};
-      let mapPin = new MapPinSvgComponent(pin, this.snapCtx, pin.position, pinDimensions);
+      let scaledPinPositon = this.getScaledPosition(pin.position);
+      let mapPin = new MapPinSvgComponent(pin, this.snapCtx, scaledPinPositon, pinDimensions);
       mapPin.draw();
     }
   }
+
+  getScaledPosition(position: Position): Position {
+    let scaledX = position.x * this.xScale;
+    let scaledY = position.y * this.yScale;
+    return { x: scaledX, y: scaledY};
+  }
+
 }
